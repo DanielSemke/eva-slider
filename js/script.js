9aeae7f2dd03eaab1fac20cbabce38faa38082cd
DanielSemke <44940344+DanielSemke@users.noreply.github.com>
2023-02-19 19:58:33 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Nav Menu
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+
+
+// Slidder Swiper
+var mySwiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  cssMode: true,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+     type: 'fraction',
+
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+
